d6411e86886c5bcae63967eece4a96e917b4aa8f
Ibanez <ibanez.soledad27@gmail.com>
2026-01-08 00:33:04 -0300

    se agrega Html proyecto X y se vincula

--- assets/js/grupo3/sole.js
+++ assets/js/grupo3/sole.js
@@ -9,3 +9,27 @@ document.addEventListener('DOMContentLoaded', () => {
         img.src='../../img/Grupo3/ibanez/sol.jpg';
     });
 });
+
+window.onload = function() {
+    const corazon = document.querySelector('.fa-heart');
+    corazon.classList.add('corazon-animado');
+};
+
+$(window).scroll(function() {
+    if ($(this).scrollTop() > 300) { // Si bajó más de 300px
+        $('#btnArriba').fadeIn();    // Aparece suavemente
+    } else {
+        $('#btnArriba').fadeOut();   // Desaparece
+    }
+});
+
+$('#btnArriba').click(function() {
+    $('html, body').animate({scrollTop: 0}, 800); // Sube con animación suave
+});
+
+$('#dark-mode-toggle').click(function() {
+    $('body').toggleClass('dark-theme'); // Quita o pone la clase "dark-theme"
+    
+    // Cambia el icono del botón
+    $(this).find('i').toggleClass('fa-moon fa-sun');
+});
